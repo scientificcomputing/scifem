d1d545970dc3d0b6f85e66c48cb79ff62e112850
Jørgen S. Dokken <dokken@simula.no>
2024-09-18 09:58:27 +0000

    Make tensor valued real spaces + add tests

--- src/scifem/__init__.py
+++ src/scifem/__init__.py
@@ -4,16 +4,17 @@ import numpy as np
 from . import _scifem
 
 
-def create_real_functionspace(mesh: dolfinx.mesh.Mesh) -> dolfinx.fem.FunctionSpace:
+def create_real_functionspace(mesh: dolfinx.mesh.Mesh, value_shape: tuple[int, ...]=()) -> dolfinx.fem.FunctionSpace:
 
     
-    ufl_e = basix.ufl.element("P", mesh.basix_cell(), 0, dtype=float, discontinuous=True)
+    ufl_e = basix.ufl.element("P", mesh.basix_cell(), 0, dtype=float, discontinuous=True,
+                              shape=value_shape)
     
     if (dtype:=mesh.geometry.x.dtype) == np.float64:
     
-        cppV = _scifem.create_real_functionspace_float64(mesh._cpp_object)
+        cppV = _scifem.create_real_functionspace_float64(mesh._cpp_object, value_shape)
     elif dtype == np.float32:
-        cppV = _scifem.create_real_functionspace_float32(mesh._cpp_object)
+        cppV = _scifem.create_real_functionspace_float32(mesh._cpp_object, value_shape)
     else:
         raise ValueError(f"Unsupported dtype: {dtype}")
     return dolfinx.fem.FunctionSpace(mesh, ufl_e, cppV)
