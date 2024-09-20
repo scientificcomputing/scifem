c3ffabf4af33e3c2eea80aad16cdd033595b2f4e
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-09-20 23:04:14 +0200

    Add code of conduct contributing guide and docs (#27)
    * Add code of conduct and contributing guide
    
    * Add documentation and update contributing guide
    
    * Add workflow for building docs
    
    * create full demo
    
    * add assemble scalar
    
    * Ruff format demo
    
    * pre-commit
    
    * Try no build isolation
    
    * Make demo compatible with 0.8.0 and 0.9.0.0
    
    * fix trailing space
    
    * Fix for parallel computing
    
    ---------
    
    Co-authored-by: Jørgen S. Dokken <dokken@simula.no>

--- src/scifem/__init__.py
+++ src/scifem/__init__.py
@@ -1,7 +1,11 @@
+from mpi4py import MPI
 import dolfinx
 import basix
 import numpy as np
+import ufl
 from . import _scifem  # type: ignore
+
+__all__ = ["create_real_functionspace", "assemble_scalar"]
 
 
 def create_real_functionspace(
@@ -32,3 +36,19 @@ def create_real_functionspace(
     else:
         raise ValueError(f"Unsupported dtype: {dtype}")
     return dolfinx.fem.FunctionSpace(mesh, ufl_e, cppV)
+
+
+def assemble_scalar(J: ufl.form.Form | dolfinx.fem.Form) -> np.floating | np.complexfloating:
+    """Assemble a scalar form and gather result across processes
+
+    Args:
+        form: The form to assemble.
+
+    Returns:
+        The accumulated value of the assembled form.
+    """
+    compiled_form = dolfinx.fem.form(J)
+    if (rank := compiled_form.rank) != 0:
+        raise ValueError(f"Form must be a scalar form, got for of arity {rank}")
+    local_result = dolfinx.fem.assemble_scalar(compiled_form)
+    return compiled_form.mesh.comm.allreduce(local_result, op=MPI.SUM)
