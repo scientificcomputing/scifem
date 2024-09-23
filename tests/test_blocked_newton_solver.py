e870d3719de811ee272661bd81bbd77010299bff
Jørgen S. Dokken <dokken@simula.no>
2024-09-23 10:45:49 +0000

    Revert fancy fixes from https://github.com/FEniCS/ufl/pull/308

--- tests/test_blocked_newton_solver.py
+++ tests/test_blocked_newton_solver.py
@@ -16,9 +16,18 @@ def test_NewtonSolver(factor):
     V = dolfinx.fem.functionspace(mesh, ("Lagrange", 1))
     Q = dolfinx.fem.functionspace(mesh, ("Lagrange", 2))
 
-    W = ufl.MixedFunctionSpace(V, Q)
-    v, q = ufl.TestFunctions(W)
-    du, dp = ufl.TrialFunctions(W)
+    # if dolfinx.__version__ == "0.8.0":
+    v = ufl.TestFunction(V)
+    q = ufl.TestFunction(Q)
+    du = ufl.TrialFunction(V)
+    dp = ufl.TrialFunction(Q)
+    # elif dolfinx.__version__ == "0.9.0.0":
+        # Relies on: https://github.com/FEniCS/ufl/pull/308
+        # W = ufl.MixedFunctionSpace(V, Q)
+        # v, q = ufl.TestFunctions(W)
+        # du, dp = ufl.TrialFunctions(W)
+    # else:
+    #     raise ValueError("Unsupported version of dolfinx")
 
     # Set nonzero initial guess
     # Choose initial guess acording to the input factor
@@ -33,16 +42,17 @@ def test_NewtonSolver(factor):
     c1 = dolfinx.fem.Constant(mesh, ftype(0.82))
     F0 = ufl.inner(c0 * u**2, v) * ufl.dx - ufl.inner(u_expr, v) * ufl.dx
     F1 = ufl.inner(c1 * p**2, q) * ufl.dx - ufl.inner(p_expr, q) * ufl.dx
-    if dolfinx.__version__ == "0.8.0":
-        F = [F0, F1]
-        J = [
-            [ufl.derivative(F0, u, du), ufl.derivative(F0, p, du)],
-            [ufl.derivative(F1, u, dp), ufl.derivative(F1, p, dp)],
-        ]
-    elif dolfinx.__version__ == "0.9.0.0":
-        F_ = F0 + F1
-        F = list(ufl.extract_blocks(F_))
-        J = ufl.extract_blocks(ufl.derivative(F_, u, du) + ufl.derivative(F_, p, dp))
+    # if dolfinx.__version__ == "0.8.0":
+    F = [F0, F1]
+    J = [
+        [ufl.derivative(F0, u, du), ufl.derivative(F0, p, dp)],
+        [ufl.derivative(F1, u, du), ufl.derivative(F1, p, dp)],
+    ]
+    # elif dolfinx.__version__ == "0.9.0.0":
+        # Relies on: https://github.com/FEniCS/ufl/pull/308
+        # F_ = F0 + F1
+        # F = list(ufl.extract_blocks(F_))
+        # J = ufl.extract_blocks(ufl.derivative(F_, u, du) + ufl.derivative(F_, p, dp))
     petsc_options = {"ksp_type": "preonly", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"}
     solver = NewtonSolver(F, J, [u, p], max_iterations=25, petsc_options=petsc_options)
     solver.solve()
