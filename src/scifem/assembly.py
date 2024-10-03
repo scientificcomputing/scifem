fb257cd7938f7da0b450906721f41c3e0e806a5d
Min RK <benjaminrk@gmail.com>
2024-10-03 12:20:39 +0200

    import annotations from future for deferred evaluation
    needed to run on Python 3.9

--- src/scifem/assembly.py
+++ src/scifem/assembly.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from mpi4py import MPI
 import ufl
 import numpy as np
--- src/scifem/mesh.py
+++ src/scifem/mesh.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import dolfinx
 import typing
 import numpy as np
