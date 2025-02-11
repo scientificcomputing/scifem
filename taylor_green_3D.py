c9893be0c4df477559af779c5f7b40b866580c0b
Jørgen S. Dokken <dokken@simula.no>
2025-02-11 15:49:38 +0100

    Simplify problem

--- taylor_green_3D.py
+++ taylor_green_3D.py
@@ -13,40 +13,35 @@ import oasisx
 
 
 class U:
-    def __init__(self, t, nu):
-        self.t = t
+    def __init__(self, nu, V0, L):
         self.nu = nu
+        self.V0 = V0
+        self.L = L
 
     def eval_x(
         self, x: npt.NDArray[np.float64]
     ) -> npt.NDArray[dolfinx.default_scalar_type]:
         return (
-            -np.cos(np.pi * x[0])
-            * np.sin(np.pi * x[1])
-            * np.cos(np.pi * x[2])
-            * np.exp(-3.0 * self.nu * np.pi**2 * float(self.t))
+            self.V0
+            * np.sin(x[0] / self.L)
+            * np.cos(x[1] / self.L)
+            * np.sin(x[2] / self.L)
         )
 
     def eval_y(
         self, x: npt.NDArray[np.float64]
     ) -> npt.NDArray[dolfinx.default_scalar_type]:
         return (
-            np.cos(np.pi * x[1])
-            * np.sin(np.pi * x[0])
-            * np.cos(np.pi * x[2])
-            * np.exp(-3.0 * self.nu * np.pi**2 * float(self.t))
+            -self.V0
+            * np.cos(x[0] / self.L)
+            * np.sin(x[1] / self.L)
+            * np.cos(x[2] / self.L)
         )
 
     def eval_z(
         self, x: npt.NDArray[np.float64]
     ) -> npt.NDArray[dolfinx.default_scalar_type]:
-        return (
-            -1
-            * np.cos(np.pi * x[2])
-            * np.sin(np.pi * x[0])
-            * np.sin(np.pi * x[1])
-            * np.exp(-3.0 * self.nu * np.pi**2 * float(self.t))
-        )
+        return np.zeros_like(x[0])
 
 
 parser = argparse.ArgumentParser(description="Taylor-Green 3D demo")
@@ -59,26 +54,12 @@ parser.add_argument(
     help="Number of elements in each direction.",
 )
 parser.add_argument(
-    "-T0",
-    "--T-start",
-    dest="T_start",
-    type=float,
-    help="Start time of simulation",
-    default=0,
-)
-parser.add_argument(
-    "-T1", "--T-end", dest="T_end", type=float, help="End time of simulation", default=1
-)
-parser.add_argument("-dt", dest="dt", type=float, help="Time step", default=0.1)
-parser.add_argument(
-    "-nu", dest="nu", type=float, help="Kinematic viscosity", default=0.01
-)
-parser.add_argument(
     "-u", dest="u_deg", type=int, help="Degree of velocity space", default=2
 )
 parser.add_argument(
     "-p", dest="p_deg", type=int, help="Degree of pressure space", default=1
 )
+parser.add_argument("-L", dest="L", type=float, help="Length of domain", default=1)
 parser.add_argument(
     "-lm",
     "--low-memory",
@@ -109,18 +90,21 @@ inputs = parser.parse_args()
 logger = logging.getLogger("oasisx")
 logger.setLevel(getattr(logging, inputs.log_level.upper(), logging.INFO))
 
-assert inputs.T_start < inputs.T_end
-T_start, T_end, dt, nu = inputs.T_start, inputs.T_end, inputs.dt, inputs.nu
-num_steps = int((T_end - T_start) // dt)
+V0 = 1.0
+L = inputs.L
+t_c = L / V0
+T_end = t_c
+RE = 1600
+nu = 1 / (RE * (V0 * L))
+dt = 0.001
+
+num_steps = int((T_end) // dt)
 
 assert inputs.u_deg > inputs.p_deg
 el_u = ("Lagrange", inputs.u_deg)
-el_p = ("Lagrange", inputs.u_deg - 1)
-import basix.ufl
-
-# el_p = el_p = basix.ufl.element("P", basix.cell.CellType.hexahedron, 1, discontinuous=True)
+el_p = ("Lagrange", inputs.p_deg)
 f = None
-options = {"low_memory_version": inputs.lm, "gamma": 10}
+options = {"low_memory_version": inputs.lm}
 
 solver_options = {
     "tentative": {
@@ -144,9 +128,9 @@ N = inputs.N
 logger.info(f"Creating mesh with {N} elements in each direction.")
 mesh = dolfinx.mesh.create_box(
     MPI.COMM_WORLD,
-    [[-1, -1, -1], [1, 1, 1]],
+    [[0, 0, 0], [2 * np.pi * L, 2 * np.pi * L, 2 * np.pi * L]],
     [inputs.N, inputs.N, inputs.N],
-    cell_type=dolfinx.mesh.CellType.hexahedron,
+    cell_type=dolfinx.mesh.CellType.tetrahedron,
 )
 # Convert mesh to periodic mesh
 L_min = [
@@ -192,26 +176,14 @@ mesh, replaced_vertices, replacement_map = create_periodic_mesh(
 )
 
 mesh.topology.create_entities(mesh.topology.dim - 1)
-print(f"NUm vertices post refinement {mesh.topology.index_map(0).size_global}")
-print(f"NUm facets post refinement {mesh.topology.index_map(2).size_global}")
-print(f"NUm cells post refinement {mesh.topology.index_map(3).size_global}")
-with dolfinx.io.XDMFFile(mesh.comm, "Periodic_mesh.xdmf", "w") as xdmf:
-    xdmf.write_mesh(mesh)
 
 dim = mesh.topology.dim - 1
 # Locate facets for boundary conditions and create meshtags
 mesh.topology.create_connectivity(dim, dim + 1)
 logger.info("Mesh created and connectivity established.")
 
-facets = dolfinx.mesh.exterior_facet_indices(mesh.topology)
-values = np.full_like(facets, 3, dtype=np.int32)
-sort = np.argsort(facets)
-facet_tags = dolfinx.mesh.meshtags(mesh, dim, facets[sort], values[sort])
-logger.info("Boundary facets located and tagged.")
 
-u_time = dolfinx.fem.Constant(mesh, dolfinx.default_scalar_type(T_start))
-p_time = dolfinx.fem.Constant(mesh, dolfinx.default_scalar_type(T_start - dt / 2.0))
-u_ex = U(t=u_time, nu=nu)
+u_ex = U(nu=nu, V0=V0, L=L)
 logger.info("Setting up boundary conditions.")
 
 bcs_u: List[List[oasisx.DirichletBC]] = [[] for _ in range(mesh.geometry.dim)]
@@ -233,26 +205,17 @@ logger.info("Solver initialized.")
 
 # Set initial conditions for velocity
 logger.info("Setting up initial conditions.")
-u_time.value = T_start - dt
 solver._u2[0].interpolate(u_ex.eval_x)
 solver._u2[1].interpolate(u_ex.eval_y)
 solver._u2[2].interpolate(u_ex.eval_z)
-u_time.value = T_start
 solver._u1[0].interpolate(u_ex.eval_x)
 solver._u1[1].interpolate(u_ex.eval_y)
 solver._u1[2].interpolate(u_ex.eval_z)
 
 # Set initial conditions for pressure
 x = ufl.SpatialCoordinate(mesh)
-man_p = (
-    -0.25
-    * (
-        ufl.cos(2 * ufl.pi * x[0])
-        + ufl.cos(2 * ufl.pi * x[1])
-        + ufl.cos(2 * ufl.pi * x[2])
-    )
-    * ufl.exp(-6 * ufl.pi**2 * nu * p_time)
-)
+man_p = (1 / 16) * (ufl.cos(2 * x[0]) + ufl.cos(2 * x[1])) * (ufl.cos(2 * x[2]) + 2)
+
 p_expr = dolfinx.fem.Expression(man_p, solver._Q.element.interpolation_points())
 solver._p.interpolate(p_expr)
 logger.info("Initial conditions set.")
@@ -260,23 +223,32 @@ logger.info("Initial conditions set.")
 
 V_out = dolfinx.fem.functionspace(mesh, ("DG", inputs.u_deg, (mesh.geometry.dim,)))
 v_out = dolfinx.fem.Function(V_out)
-vtxu = dolfinx.io.VTXWriter(mesh.comm, "u.bp", [v_out], engine="BP5")
+
+vtxu = dolfinx.io.VTXWriter(
+    mesh.comm,
+    "u.bp",
+    [v_out],
+    engine="BP5",
+    mesh_policy=dolfinx.cpp.io.VTXMeshPolicy.reuse,
+)
 
 
-Q_out = dolfinx.fem.functionspace(mesh, ("DG", inputs.u_deg))
-q_out = dolfinx.fem.Function(Q_out)
-q_out.name = "phi"
-vtxp = dolfinx.io.VTXWriter(mesh.comm, "p.bp", [q_out], engine="BP5")
+vtxp = dolfinx.io.VTXWriter(
+    mesh.comm,
+    "p.bp",
+    [solver._p],
+    engine="BP5",
+    mesh_policy=dolfinx.cpp.io.VTXMeshPolicy.reuse,
+)
+t = 0
 for i in range(num_steps):
-    u_time.value += dt
-    p_time.value += dt
-    print(float(u_time.value))
-    logger.debug(f"Time step {i + 1}/{num_steps}, solving at t={u_time.value:.3f}.")
+    print(f"{i}/{num_steps}", end="\r")
+    t += float(dt)
+    logger.debug(f"Time step {i + 1}/{num_steps}, solving at t={t:.3f}.")
     solver.solve(dt, nu, max_iter=1)
     v_out.interpolate(solver.u)
-    vtxu.write(u_time.value)
-    q_out.interpolate(solver._dp)
-    vtxp.write(p_time.value)
+    vtxu.write(t)
+    vtxp.write(t)
 
 vtxu.close()
 vtxp.close()
